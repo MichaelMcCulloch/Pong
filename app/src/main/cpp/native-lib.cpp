3d5bcfacab0305a81f819d1488213a8aba794e2e
Michael McCulloch <michael.james.mcculloch@gmail.com>
2018-12-22 23:24:46 -0700

    Switch to GLES3 and use shaders

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -25,7 +25,7 @@
 #include <cassert>
 
 #include <EGL/egl.h>
-#include <GLES/gl.h>
+#include <GLES3/gl3.h>
 
 #include <android/sensor.h>
 #include <android/log.h>
@@ -70,10 +70,93 @@ struct engine {
     EGLDisplay display;
     EGLSurface surface;
     EGLContext context;
+    GLuint shader;
     int32_t width;
     int32_t height;
     struct saved_state state;
 };
+
+bool CheckGLErrors() {
+    bool error = false;
+    for (GLenum flag = glGetError(); flag != GL_NO_ERROR; flag = glGetError()) {
+        LOGW("OpenGL ERROR:");
+        switch (flag) {
+            case GL_INVALID_ENUM:
+                LOGW("GL_INVALID_ENUM");
+                break;
+            case GL_INVALID_VALUE:
+                LOGW("GL_INVALID_VALUE");
+                break;
+            case GL_INVALID_OPERATION:
+                LOGW("GL_INVALID_OPERATION");
+                break;
+            case GL_INVALID_FRAMEBUFFER_OPERATION:
+                LOGW("GL_INVALID_FRAMEBUFFER_OPERATION");
+                break;
+            case GL_OUT_OF_MEMORY:
+                LOGW("GL_OUT_OF_MEMORY");
+                break;
+            default:
+                LOGW("[unknown error code]");
+        }
+        error = true;
+    }
+    return error;
+}
+
+
+// creates and returns a shader object compiled from the given source
+GLuint CompileShader(GLenum shaderType, const std::string &source) {
+    // allocate shader object name
+    GLuint shaderObject = glCreateShader(shaderType);
+
+    // try compiling the source as a shader of the given type
+    const GLchar *source_ptr = source.c_str();
+    glShaderSource(shaderObject, 1, &source_ptr, 0);
+    glCompileShader(shaderObject);
+
+    // retrieve compile status
+    GLint status;
+    glGetShaderiv(shaderObject, GL_COMPILE_STATUS, &status);
+    if (status == GL_FALSE) {
+        GLint length;
+        glGetShaderiv(shaderObject, GL_INFO_LOG_LENGTH, &length);
+        std::string info(length, ' ');
+        glGetShaderInfoLog(shaderObject, info.length(), &length, &info[0]);
+        LOGW("ERROR compiling shader:\n\n %s \n %s", source.c_str(), info.c_str());
+
+    }
+
+    return shaderObject;
+}
+
+// creates and returns a program object linked from vertex and fragment shaders
+GLuint LinkProgram(GLuint vertexShader, GLuint fragmentShader) {
+    // allocate program object name
+    GLuint programObject = glCreateProgram();
+
+    // attach provided shader objects to this program
+    if (vertexShader)
+        glAttachShader(programObject, vertexShader);
+    if (fragmentShader)
+        glAttachShader(programObject, fragmentShader);
+
+    // try linking the program with given attachments
+    glLinkProgram(programObject);
+
+    // retrieve link status
+    GLint status;
+    glGetProgramiv(programObject, GL_LINK_STATUS, &status);
+    if (status == GL_FALSE) {
+        GLint length;
+        glGetProgramiv(programObject, GL_INFO_LOG_LENGTH, &length);
+        std::string info(length, ' ');
+        glGetProgramInfoLog(programObject, info.length(), &length, &info[0]);
+        LOGW("ERROR linking shader program: \n %s", info.c_str());
+    }
+
+    return programObject;
+}
 
 /**
  * Initialize an EGL context for the current display.
@@ -159,10 +242,32 @@ static int engine_init_display(struct engine *engine) {
         LOGI("OpenGL Info: %s", info);
     }
     // Initialize GL state.
-    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_FASTEST);
     glEnable(GL_CULL_FACE);
-    glShadeModel(GL_SMOOTH);
     glDisable(GL_DEPTH_TEST);
+
+    //prepare the shaders
+    std::string vertexSource = "attribute vec4 vPosition;   \n"
+                               "void main()                 \n"
+                               "{"
+                               "    gl_Position=vPosition;  \n"
+                               "}";
+    std::string fragmentSource = "precision mediump float;                   \n"
+                                 "void main()                                \n"
+                                 "{                                          \n"
+                                 "  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); \n"
+                                 "}                                          \n";
+    if (vertexSource.empty() || fragmentSource.empty())
+        return -1;
+
+    auto vertex = CompileShader(GL_VERTEX_SHADER, vertexSource);
+    auto fragment = CompileShader(GL_FRAGMENT_SHADER, fragmentSource);
+    auto program = LinkProgram(vertex, fragment);
+
+    if (CheckGLErrors()) {
+        return -1;
+    } else {
+        engine->shader = program;
+    }
 
     return 0;
 }
