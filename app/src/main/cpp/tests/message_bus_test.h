b5f5397f35e6674493c4529fd525b6ef92581225
MichaelMcCulloch <michael.james.mcculloch@gmail.com>
2019-01-01 15:22:49 -0700

    Integrate gmock

--- app/src/main/cpp/tests/message_bus_test.h
+++ app/src/main/cpp/tests/message_bus_test.h
@@ -7,12 +7,30 @@
 #include "../MessageBus/MessageBus.h"
 #include "../logging.h"
 
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+using ::testing::AtLeast;
+
+class MockBusNode : public BusNode {
+public:
+    MOCK_METHOD0(update,
+                 void());
+private:
+    void onNotify(Message message) {
+
+        switch (message.messageType) {
+            default:
+                return;
+        }
+    }
+};
+
 TEST(messageBusTest1,
      pass) {
-    MessageBus msg_bs = MessageBus();
-}
+    MockBusNode busNode;
+    EXPECT_CALL(busNode,
+                update()).Times(AtLeast(1));
 
-TEST(messageBusTest2,
-     fail) {
 
 }
--- app/src/main/cpp/tests/testDriver.cpp
+++ app/src/main/cpp/tests/testDriver.cpp
@@ -1,17 +1,2 @@
-#include "gtest/gtest.h"
 
-#include "message_bus_test.h"
-#include "renderer_test.h"
-#include "engine_test.h"
 
-TEST(adder,
-     pass) {
-    EXPECT_EQ(42,
-              40 + 2);
-}
-
-TEST(foo,
-     fail) {
-    EXPECT_EQ(42,
-              42 + 1);
-}
